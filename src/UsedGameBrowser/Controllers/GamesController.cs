5022279be814c43d79c1e929baeb89fd4b286c8d
Will Johnson <trashmoldwilliams@gmail.com>
2016-04-22 09:10:46 -0700

    Add functionality to edit and delete games through the UI.

--- src/UsedGameBrowser/Controllers/GamesController.cs
+++ src/UsedGameBrowser/Controllers/GamesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using UsedGameBrowser.Models;
+using Microsoft.Data.Entity;
 
 namespace UsedGameBrowser.Controllers
 {
@@ -30,6 +31,35 @@ namespace UsedGameBrowser.Controllers
         public IActionResult Create(Game game)
         {
             db.Games.Add(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisGame = db.Games.FirstOrDefault(games => games.GameId == id);
+            return View(thisGame);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Game game)
+        {
+            db.Entry(game).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisGame = db.Games.FirstOrDefault(items => items.GameId == id);
+            return View(thisGame);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisGame = db.Games.FirstOrDefault(items => items.GameId == id);
+            db.Games.Remove(thisGame);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
