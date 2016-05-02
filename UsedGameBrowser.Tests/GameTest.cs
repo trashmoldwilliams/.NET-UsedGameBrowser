b56f860f28c5940bc631361da18b51963a883e59
Andrew Cho and Will Johnson <trashmoldwilliams@gmail.com>
2016-05-02 15:27:20 -0700

    Add controller testing.

--- UsedGameBrowser.Tests/GameTest.cs
+++ UsedGameBrowser.Tests/GameTest.cs
@@ -1,42 +1,49 @@
-﻿using System;
+﻿using Microsoft.AspNet.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsedGameBrowser.Controllers;
 using UsedGameBrowser.Models;
 using Xunit;
 
 namespace UsedGameBrowser.Tests
 {
-    public class GameTest
+    public class GamesControllerTest
     {
         [Fact]
-        public void GetTitleTest()
+        public void Post_MethodAddsGames_Test()
         {
-            //Arrange
-            var game = new Game();
-            game.Title = "Super Metroid";
+            // Arrange
+            GamesController controller = new GamesController();
+            Game testGame = new Game();
+            Platform testPlatform = new Platform();
 
-            //Act
-            var result = game.Title;
 
-            //Assert
-            Assert.Equal("Super Metroid", result);
+            testPlatform.PlatformId = 1;
+            testPlatform.Name = "Console";
+            testPlatform.Description = "Xbox";
+            testPlatform.AveragePrice = 15;
+
+            testGame.Title = "SSB";
+            testGame.AveragePrice = 10;
+            testGame.Description = "Awesome game super broken as link.";
+            testGame.PlatformId = 1;
+
+            testGame.Platform = testPlatform;
+
+
+            // Act
+            controller.Create(testGame);
+            ViewResult indexView = new GamesController().Index() as ViewResult;
+            var collection = indexView.ViewData.Model as IEnumerable<Game>;
+
+            // Assert
+            Assert.Contains<Game>(testGame, collection);
         }
-
-        [Fact]
-        public void GetPriceTest()
-        {
-            //Arrange
-            var game = new Game();
-            game.AveragePrice = 10;
-
-            //Act
-            var result = game.AveragePrice;
-
-            //Assert
-            Assert.Equal(10, result);
-        }
-
 
     }
 }
+
+
+
--- src/UsedGameBrowser/Models/Game.cs
+++ src/UsedGameBrowser/Models/Game.cs
@@ -17,5 +17,23 @@ namespace UsedGameBrowser.Models
         public int AveragePrice { get; set; }
         public int PlatformId { get; set; }
         public virtual Platform Platform { get; set; }
+
+        public override bool Equals(System.Object otherGame)
+        {
+            if (!(otherGame is Game))
+            {
+                return false;
+            }
+            else
+            {
+                Game newGame = (Game)otherGame;
+                return this.GameId.Equals(newGame.GameId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GameId.GetHashCode();
+        }
     }
 }
